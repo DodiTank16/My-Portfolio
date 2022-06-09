1f634e7e9ce467c98ca988dae06b5c4a87ec53f3
TankDodi16 <tankdodi@gmail.com>
2022-06-09 17:24:02 +0530

    initial stage

---
+++ src/components/constants/MainContent.js
@@ -0,0 +1,45 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import logo from "../../images/logo.png";
+
+function MainContent() {
+	return (
+		<>
+			<div className="row">
+				<div className="column banner-content">
+					<Typography
+						variant="h3"
+						align="center"
+						sx={{
+							mr: 2,
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "white",
+							textDecoration: "none",
+						}}>
+						CREATIVE DEVELOPER
+					</Typography>
+					<Typography
+						variant="h3"
+						align="center"
+						sx={{
+							mr: 2,
+							fontFamily: "monospace",
+							fontWeight: 400,
+							letterSpacing: ".3rem",
+							color: "white",
+							textDecoration: "none",
+						}}>
+						Sensible To Fullstack
+					</Typography>
+				</div>
+				<div className="column banner-logo">
+					<img src={logo} alt="" />
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default MainContent;
