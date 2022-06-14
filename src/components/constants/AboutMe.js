9640734883d42ec683dcacff1bb1468766f55a63
TankDodi16 <tankdodi@gmail.com>
2022-06-14 17:40:48 +0530

    About section added

--- src/components/constants/AboutMe.js
+++ src/components/constants/AboutMe.js
@@ -1,26 +1,60 @@
-import { Typography } from "@mui/material";
 import React from "react";
-import logo from "../../images/tringle-logo.png";
 
 function MainContent() {
 	return (
 		<>
 			<div className="container">
-				<div>
-					<Typography
-						variant="h3"
-						align="left"
-						sx={{
-							mr: 2,
-							fontFamily: "Eczar",
-							fontWeight: 400,
-							letterSpacing: ".2rem",
-							color: "white",
-							textDecoration: "none",
-							textAlign: "center",
-						}}>
-						About Me{" "}
-					</Typography>
+				<div className="section-two">
+					<div className="about-heading title-section">
+						<h1>
+							ABOUT <span>ME</span>
+						</h1>
+						<span class="title-bg">Resume</span>
+					</div>
+					<div className="aboutme">
+						<div className="about-table">
+							<table border={1}>
+								<tr>
+									<td colSpan={2}>
+										<h3>PERSONAL INFOS</h3>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										First Name: <strong>Dodi</strong>
+									</td>
+									<td>
+										Last Name: <strong> Tank</strong>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Age: <strong>21 Years</strong>
+									</td>
+									<td>
+										Nationality: <strong>Indian</strong>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Address: <strong>Rajkot, Gujarat</strong>
+									</td>
+									<td>
+										Email: <strong>tankdodi@gmail.com</strong>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Skype: <strong>tank.dodi</strong>
+									</td>
+									<td>
+										Languages: <strong>English, Gujarati, Hindi</strong>
+									</td>
+								</tr>
+							</table>
+						</div>
+						<div className="cards">Hello World</div>
+					</div>
 				</div>
 			</div>
 		</>
